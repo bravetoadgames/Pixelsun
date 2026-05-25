244a115b8ed1d62d1a9d8aab552c172742e597e5
Arjen Schumacher <arjen.schumacher@gmail.com>
2026-05-25 11:14:07 +0200

    Configurable GUI which saves settings immediately.

--- compile.sh
+++ compile.sh
@@ -3,6 +3,6 @@ rm pixelsun
 pyinstaller --onefile pixelsun.py
 staticx dist/pixelsun dist/pixelsun_static
 mv dist/pixelsun_static ./pixelsun
-tar -czf pixelsun-1.0-ubuntu-x86_64.tar.gz pixelsun config.txt tom-thumb.ttf
+tar -czf pixelsun-1.0-ubuntu-x86_64.tar.gz pixelsun config.txt settings.txt tom-thumb.ttf
 
 
